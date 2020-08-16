2e53ab055d645388979b8a38c4e8d2a0af17bcd0
HassanFouaad <mrhassanfouad1@gmail.com>
2020-08-16 21:02:07 +0200

    deploy

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   ///set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
